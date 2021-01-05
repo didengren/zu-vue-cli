a724d801509f457f5c032491808461dd590cbe75
nianzu.xu@trinasolar.com <nianzu.xu@trinasolar.com>
2021-01-05 11:26:46 +0800

    修复已存在非空文件夹不能初始化脚手架的问题

--- tempProj/src/http/portal.js
+++
@@ -1,9 +0,0 @@
-// import { post, get } from "./AxiosInsImpl";
-
-// 开发环境交换token
-export const devTokenAPI = (username, password) => {};
-// 正式环境交换token
-export const getTokenAPI = (userName, password) => {};
-
-// 用户信息
-export const userMsgAPI = () => {};
--- utils/index.js
+++ utils/index.js
@@ -3,6 +3,8 @@ const path = require("path");
 
 exports.isDirectory = (dir) => fs.lstatSync(dir).isDirectory();
 
+exports.isFile = (path) => fs.lstatSync(path).isFile();
+
 exports.nonEmptyDir = (dir) => {
   try {
     if (fs.readdirSync(dir).length > 0) return true;
